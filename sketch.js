43a4ba2dc1292ce94adeae43c0ec17c82a6b496f
Dhanvinsh <72690343+Dhanvinsh99@users.noreply.github.com>
2022-10-29 16:50:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
-var bg, bgImg, player, shooterImg, shootingImg, zombie, zombieImg
+var bg, bgImg, player, shooterImg, shootingImg, zombie, zombieImg, bullet, zombieGroup, bulletGroup; 
+var bulletScore = 80;
 function preload(){
     bgImg = loadImage("assets/bg.jpeg")
     shooterImg = loadImage("assets/shooter_2.png")
@@ -14,25 +15,46 @@ function setup(){
     player = createSprite(displayWidth-1500, displayHeight-400, 50,50)
     player.addImage(shooterImg)
     player.scale = 0.55
+    zombieGroup = createGroup();
+    bulletGroup = createGroup();
 }
 
 function draw(){
     background('#FF2400')
+    //text(variblename,x,y)
     if(keyDown("up")){ 
       player.y -= 30   
     }
     if(keyDown("down")){
         player.y += 30
     }
-    if(keyWentDown("space")){
+    //a simple statement that releases the bullets and changes the image of the shooter to the shooting postions when space key is pressed
+    if(keyWentDown("space")&& bulletScore >=0){
         player.addImage(shootingImg)
+        bullet = createSprite(displayWidth-1400, player.y-49,20,10)
+        bullet.velocityX = 20
+        bullet.depth = zombie.depth - 4
+        bullet.lifetime = 400
+        bulletScore -= 1
+        bulletGroup.add(bullet)
     }
     if(keyWentUp("space")){
         player.addImage(shooterImg)
     }
+    if(bulletGroup.isTouching(zombieGroup)){
+       for(i=0;i<zombieGroup.length;i++){
+        if(zombieGroup[i].isTouching(bulletGroup)){
+            zombieGroup[i].destroy()
+            bulletGroup.destroyEach()
+        }
+       }
+    }
+
     spawn_zombies()
-    
     drawSprites()
+    textSize(27.2)
+    fill("#000000")
+    text("ammo left:"+bulletScore,displayWidth-500,displayHeight-1000)
 }
 
 function spawn_zombies(){
@@ -42,5 +64,7 @@ function spawn_zombies(){
         zombie.scale = 0.30
         zombie.velocityX = -10
         zombie.lifetime = 500
+        zombieGroup.add(zombie)
     }
 }
+
